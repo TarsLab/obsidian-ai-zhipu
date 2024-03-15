2465feadb1b891989e12410620879569b59c45b0
xusijie <ae86jack@gmail.com>
2024-03-15 18:27:35 +0800

    Update prompt templates for English and Chinese versions

--- src/promptZh.ts
+++ src/promptZh.ts
@@ -3,13 +3,13 @@ import dedent from 'ts-dedent'
 export const promptZh = dedent`
 # 提示词模版
 
-- If you want to use english prompt, please update setting in obsidian-ai-zhipu plugin.
-- 本文件按照幻灯片格式，用“---”来分隔每一页的内容
-- 第一页是说明，后面是正式的模板内容
-- \`##\` 是标题
+- If you wish to use English prompts, please adjust the settings accordingly.
+- 本文件按照 obsidian 的幻灯片格式，用“---”来分隔每一页的内容
+- 第一页是说明，后面的每一页都是一个提示词模板
+- \`##\` 是标题。当启动命令“生成内容”，标题显示在弹窗列表中
 - \`%%\`里面是技术参数，参考[智谱AI的接口文档](https://open.bigmodel.cn/dev/api)
--  在✨💡和💡✨之间的是模板内容，插件会把 \`{{selection}}\`替换为选中的文本，得到最终的提示词
-- 如果你想查看最终的提示词是否符合预期，或者更多技术参数，使用插件的“显示上次请求参数”功能
+- 在✨💡和💡✨之间的文本是模板内容，插件会把 \`{{selection}}\`替换为选中的文本，得到最终的提示词
+- 如果你想查看对话的最终提示词是否符合预期，或者更多技术参数，启动命令“显示对话详情”
 
 ---
 
